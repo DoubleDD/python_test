f552c03d8c87fae2355fe12c0c039999334fe01b
kedong <1150744690@qq.com>
2020-04-14 17:06:24 +0800

    bugfixed

--- selenium/zxy_test.py
+++ selenium/zxy_test.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python
 # -*- coding:utf-8 -*-
-import unittest
 import time
-
+from unittest import TestCase, main
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class ZxyTestCase(unittest.TestCase):
+class ZxyTestCase(TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome()
 
@@ -23,7 +21,11 @@ class ZxyTestCase(unittest.TestCase):
     def testLogin(self):
         driver = self.driver
         findXpath = self.findByXpath
-        driver.get("https://dev9.zhixueyun.com")
+        try:
+            driver.get("https://kedong.zhixueyun.com")
+        except:
+            print("加载页面太慢，停止加载，继续下一步操作")
+            driver.execute_script("window.stop()")
         driver.maximize_window()
         print('打开url')
         sleep(2)
@@ -34,60 +36,18 @@ class ZxyTestCase(unittest.TestCase):
         print('点击登录按钮')
         sleep(1)
 
-        username = findXpath('//*[@id="D35username"]')
-        password = findXpath('//*[@id="D35pword"]')
+        username = driver.find_element_by_name('username')
+        password = driver.find_element_by_name('pword')
         username.send_keys('admin' + Keys.ESCAPE)
         print('设置用户名')
         password.send_keys('zxy123456' + Keys.RETURN)
         print('设置密码')
         sleep(2)
 
-        # 设置按钮
-        setting_btn = findXpath('//*[@id="D17top-content"]/div[2]/div[2]/div[3]/a/i')
-        setting_btn.click()
-        print('打开后台页面')
-        sleep(1)
-
-        # 系统管理
-        system_manage_btn = findXpath('//*[@id="D23menu-007"]')
-        system_manage_btn.click()
-        sleep()
-
-        # 系统配置
-        system_config = findXpath('//*[@id="D32menu-007003"]')
-        system_config.click()
-        rule_config_memu = findXpath('//*[@id="D32sub-menu-007003001"]')
-        rule_config_memu.click()
-        sleep(1)
-
-        # 游戏设置
-        game_config_trs = driver.find_elements_by_tag_name('tr')
-        for tr in game_config_trs:
-            tds = tr.find_elements_by_tag_name('td')
-            if tds:
-                desc = tds[1].text
-                if '游戏等级设置' == desc:
-                    setting_link = tds[3].find_element_by_link_text('设置')
-                    setting_link.click()
-                    break
-        sleep()
-        add_btn = findXpath('//*[@id="D414add-game-settings"]')
-        add_btn.click()
-        dialog = driver.find_element_by_class_name('dialog-content-main')
-        trs = dialog.find_elements_by_tag_name('tr')
-        last_tr = trs[len(trs)-1]
-        level_tds = last_tr.find_elements_by_tag_name('td')
-        level_name = level_tds[0]
-        level_icon = level_tds[1]
-        level_num = level_tds[2]
-        done_btn = level_tds[3]
-        level_name.find_element_by_tag_name('input').send_keys('test' + Keys.ESCAPE)
-        level_num.find_element_by_tag_name('input').send_keys('80000' + Keys.ESCAPE)
-        level_icon.find_element_by_link_text('上传图标（132*80）').click()
-        done_btn.find_element_by_link_text('完成').click()
-        print('done!')
-
 
 def sleep(s=.5) -> None:
     time.sleep(s)
 
+
+if __name__ == "__main__":
+    main(verbosity=2)
